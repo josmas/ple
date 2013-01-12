3d1718180c3e96b82e56e12c3dc7407b2ee71968
josmas <josmasflores@gmail.com>
2013-01-12 14:29:23 +0000

    removes ng view cause of conflicts with chrome version

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
-
-
-var PleApp = angular.module('PleApp', []);
-
-PleApp
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.
-        when('/', {templateUrl: 'partials/home.html',   controller: MainCtrl}).
-        otherwise({redirectTo: '/'});
-  }]);
-
-function MainCtrl($scope, $http) {
-  $scope.title = 'Home';
-  $http.get('https://p2pu.org/api/alpha/courses/?format=json').success(function(response) {
-    sc.response = response;
-    console.log(response);
-  });
+function MainCtrl($scope) {
+  $scope.test = 'Home';
 }

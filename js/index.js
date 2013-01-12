fe47f279be4797f9faa49410911f5ce73747e0e7
Devnook <devnook@gmail.com>
2013-01-12 14:55:00 +0000

    Add printing list of courses from p2pu

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ function MainCtrl($scope, $http) {
   $scope.title = 'Home';
   $http.get('https://p2pu.org/api/alpha/courses/?format=json').success(function(response) {
     $scope.p2pu = angular.fromJson(response);
+    console.log($scope.p2pu)
   });
   $http.get('https://www.coursera.org/maestro/api/topic/list').success(function(response) {
     $scope.coursera = response;

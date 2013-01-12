342292bdd2722d551cefb8159fdfa84a72a2b4fe
josmas <josmasflores@gmail.com>
2013-01-12 17:21:01 +0000

    first go at improving offline connectivity

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
 function MainCtrl($scope, $http) {
-  $scope.title = 'Home';
-  $http.get('https://p2pu.org/api/alpha/courses/?format=json').success(function(response) {
-    $scope.p2pu = angular.fromJson(response);
-    console.log($scope.p2pu)
-  });
+  $scope.p2pu = [];
+  if (navigator.onLine) { //Grab from the network if online
+    $http.get('https://p2pu.org/api/alpha/courses/?format=json').success(function(response) {
+      $scope.p2pu = angular.fromJson(response.objects);
+      console.log($scope.p2pu)
+    });
+  }
+  else {
+    //App is offline; look for info locally
+    chrome.storage.local.get('p2pu', function(p2puLocal) {
+      $scope.p2pu = angular.fromJson(p2puLocal.p2pu);
+      console.log('p2pu from local is: ', $scope.p2pu);
+      //telling Angular that a change in the model has happened from an async
+      //call
+      $scope.$apply(function($scope) {}); 
+    });
+  }
+
 //  TODO (Jose) this call works but we are not using it for now
 //  $http.get('https://www.coursera.org/maestro/api/topic/list').success(function(response) {
 //    $scope.coursera = response;
@@ -12,13 +25,13 @@ function MainCtrl($scope, $http) {
   $scope.saveCourses= function(courses){
     var p2puCourses = [],
         courseToStore;
-    angular.forEach($scope.p2pu.objects, function(course){
-      courseToStore = { name: course.name, description: course.short_description };
+    angular.forEach($scope.p2pu, function(course){
+      courseToStore = { name: course.name, short_description: course.short_description };
       console.log(courseToStore);
       p2puCourses.push(courseToStore);
     });
 
-    chrome.storage.sync.set({'p2pu': p2puCourses}, function() {
+    chrome.storage.local.set({'p2pu': p2puCourses}, function() {
       var notification = webkitNotifications.createNotification(
         '../img/calculator-16.png',
         'Saved!',

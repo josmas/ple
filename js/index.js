e6a6acd6b357e004c322fefdd6b7b3f55d003c5e
Devnook <devnook@gmail.com>
2013-01-12 14:31:58 +0000

    Adding htpp requests to p2pu

--- js/index.js
+++ js/index.js
@@ -1,18 +1,9 @@
-
-
-var PleApp = angular.module('PleApp', []);
-
-PleApp
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.
-        when('/', {templateUrl: 'partials/home.html',   controller: MainCtrl}).
-        otherwise({redirectTo: '/'});
-  }]);
-
 function MainCtrl($scope, $http) {
   $scope.title = 'Home';
   $http.get('https://p2pu.org/api/alpha/courses/?format=json').success(function(response) {
-    sc.response = response;
-    console.log(response);
+    $scope.p2pu = response;
+  });
+  $http.get('https://www.coursera.org/maestro/api/topic/list').success(function(response) {
+    $scope.coursera = response;
   });
 }

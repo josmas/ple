7a4bb76e50c4d76852d784dbb0a48a466f5f0f55
Devnook <devnook@gmail.com>
2013-01-12 13:29:35 +0000

    Adding angular main controller.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-console.log(angular);
+
+
+var PleApp = angular.module('PleApp', []);
+
+PleApp
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+        when('/', {templateUrl: 'partials/home.html',   controller: MainCtrl}).
+        otherwise({redirectTo: '/'});
+  }]);
+
+function MainCtrl($scope) {
+  $scope.title = 'Home';
+}
